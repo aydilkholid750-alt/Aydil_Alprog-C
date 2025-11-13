24f14f217c2e64e3661825f534004de80a2f1247
Aydil_Alprog-C <aydilkholid750@gmail.com>
2025-11-13 20:25:39 +0800

    Add files via upload

---
+++ meet2.py
@@ -0,0 +1,41 @@
+
+
+x = 10
+
+x = 10
+y = 6 lebar = 50
+
+
+print("nilai dari x =", x)
+print("nilai dari y =", y)
+print("nilai dari lembar =", lembar)
+
+# penamaan variabel
+jumlah_z = 9
+# jika lebih dari satu kara, digabung dengan underscore
+bet99 =100000 # tidak bolehb di awali angka
+
+
+#pemanggilan kedua
+print("nilai dari x =", x)
+x = 99
+print("nilai dari x =", x)
+
+
+# pemanggil indirect
+a = x
+b = lembar
+print("nilai dari a =", a)
+print("nilai dari b =", b)
+
+# nama saya = {aydil kholid}
+# nim saya = {25241070}
+
+nama = "aydil kholid"
+nim = 25241070
+
+print("nama sy adalah =", nama)
+print("nim sy adalah =", nim)
+
+
+
